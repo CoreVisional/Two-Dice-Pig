3988d12ad263ef980db92a68ea1003f513f81484
Alex Chiew <alex.chiewyz@gmail.com>
2022-01-01 23:49:56 +0800

    Update two_dice_pig.py

--- two_dice_pig.py
+++ two_dice_pig.py
@@ -1,60 +1,63 @@
 
-import names
+"""A simple CLI-based dice roll simulation game."""
 
 from random import sample
 
-from typing import Union
+import names
 
 
 def ask_user_yes_no(yes_no_question) -> bool:
-    """Simplifies if/else to determine the correct answers from the user input.
+    """Simplify if/else to determine the correct answers from the user input.
 
     Args:
         yes_no_question: A string that asks user a yes or no question.
 
     Returns:
-        True if the user's answer is in CHOICE_YES,
+        True if the user's answer is in choice_yes,
         and False otherwise.
 
         Prints a message to the user if their input are not similar
-        to the ones in CHOICE_YES and CHOICE_NO.
+        to the ones in choice_yes and choice_no.
 
     """
-    CHOICE_YES = ("yes", 'y')
-    CHOICE_NO = ("no", 'n')
+    choice_yes = ("yes", 'y')
+    choice_no = ("no", 'n')
 
     while True:
         user_choice = input(yes_no_question).lower()
 
-        if user_choice in CHOICE_YES:
+        if user_choice in choice_yes:
             return True
-        elif user_choice in CHOICE_NO:
+
+        if user_choice in choice_no:
             return False
-        else:
-            print("\nInvalid Input. Try again.")
+
+        print("\nInvalid Input. Try again.")
 
 
 def greet_user() -> None:
-    """Displays a greeting for the game Two-Dice Pig."""
+    """Display a greeting for the game Two-Dice Pig."""
     print("\n\nWelcome to Two-Dice Pig Match!")
 
 
-def print_names(p1, p2) -> None:
-    """Displays the name of the player and opponent."""
-    print(f"\n\nYour Name: {p1}\n{'-' * 23}")
-    print(f"Opponent's Name: {p2}\n")
+def print_names(p_1, p_2) -> None:
+    """Display the name of the player and opponent."""
+    print(f"\n\nYour Name: {p_1}\n{'-' * 23}")
+    print(f"Opponent's Name: {p_2}\n")
 
 
-def get_player_names() -> Union[tuple[str, str]]:
-    """Asks the user to enter their name and generates a random
-    name if the user do not wish to give player 2 a name.
+def get_player_names():
+    """Ask the user to input their name.
+
+    The program will generate a random name if the
+    user do not wish to give player 2 a name.
 
     Returns:
         A tuple of string containing the names
         of both player 1 and player 2.
 
-    This is repeated until the user has provided a 
-    name and an AI name to the program.
+    This process is repeated until the user has provided
+    a name and an AI name to the program.
 
     """
     while True:
@@ -85,7 +88,7 @@ def get_player_names() -> Union[tuple[str, str]]:
 
 
 class TwoDicePig:
-    """Begins the game and prints out the winner.
+    """Begin the game and prints out the winner.
 
     Args:
         p1_human: Indicates whether it is a human player.
@@ -96,21 +99,20 @@ class TwoDicePig:
 
     Attributes:
         player_1_name, player_2_name (str): The names of both players.
-        dice (Dice): An instance of Dice class.
+
         player_1, player_2 (Player): Instances of the Player class.
 
     """
 
     def __init__(self, p1_human: bool = True, p2_human: bool = False) -> None:
         self.player_1_name, self.player_2_name = get_player_names()
-        self.dice = Dice()
         self.player_1 = Player(f"{self.player_1_name}", p1_human)
         self.player_2 = Player(f"{self.player_2_name}", p2_human)
 
     def play(self) -> None:
-        """Starts and continue the game if a player has not scored over 100 yet.
+        """Start and continue the game if a player has not scored over 100 yet.
 
-        Calls print_winner method when a player scored over 100.
+        Call print_winner method when a player scored over 100.
 
         """
         while (self.player_1.total_score < 100 and self.player_2.total_score < 100):
@@ -120,8 +122,8 @@ class TwoDicePig:
         self.print_winner()
 
     def print_winner(self) -> None:
-        """Prints out the winner."""
-        if (self.player_1.total_score > self.player_2.total_score):
+        """Print out the winner."""
+        if self.player_1.total_score > self.player_2.total_score:
             print(
                 f"\n\033[1m{self.player_1_name.upper() + ' WINS!':^30}\033[0m")
         else:
@@ -131,37 +133,15 @@ class TwoDicePig:
         self.print_result()
 
     def print_result(self) -> None:
-        """Prints out the names of the players and the total points scored
-        by each player.
-
-        """
+        """Print out the player's name and total points scored by each player."""
         p1_final_points = f"{self.player_1_name}: {self.player_1.total_score}"
         p2_final_points = f"{self.player_2_name}: {self.player_2.total_score}"
 
         print(f"\n\n {p1_final_points} Points | {p2_final_points} Points")
 
 
-class Dice:
-    """Sets the sides count of a die to 6.
-
-    Attribute:
-        sides (int): The sides count of a die.
-
-    """
-
-    def __init__(self) -> None:
-        self.sides = 6
-
-    def roll(self):
-        """Rolls a list of two randomly selected numbers that starts
-        from 1 to 6. Each number represents the value of the die.
-        """
-        self.faces = sample(range(1, self.sides), 2)
-
-
 class Player():
-    """Sets up the turns for player 1 and player 2,
-    and get the total score once a round is over.
+    """Set up the turns and get total score after each round.
 
     Args:
         players_name: The names of both players.
@@ -180,7 +160,9 @@ class Player():
 
         total_score (int): The total score of the players after the end of a round.
 
-        dice (Dice): An instance of Dice class.
+        die_1 (None): The minimum integer value of the dice.
+
+        die_2 (None): The maximum integer value of the dice.
 
     """
 
@@ -189,27 +171,28 @@ class Player():
         self.is_human = human_player
         self.total_round = 0
         self.total_score = 0
-        self.dice = Dice()
+        self.die_1 = None
+        self.die_2 = None
 
-    def get_dice_min_max(self) -> Union[tuple[int, int]]:
-        """Finds the minimum and maximum numbers of the dice.
-
-        Calls an instance method, roll(), to get the minimum 
-        and maximum values of the dice.
+    def get_dice_min_max(self) -> tuple[int]:
+        """Set the minimum and maximum numbers of the dice.
 
         Returns:
-            A tuple of integers containing the lowest and highest
-            values of the dice.
+            A tuple of integers containing the lowest
+            and highest face values of the dice.
 
         """
-        self.dice.roll()
-        self.die_1 = min(self.dice.faces)
-        self.die_2 = max(self.dice.faces)
+        die_sides = 6
+        dice_face_values = sample(range(1, die_sides), 2)
+        self.die_1 = min(dice_face_values)
+        self.die_2 = max(dice_face_values)
         return self.die_1, self.die_2
 
     def turns(self) -> None:
-        """Switches the turn to the actual player if is_human is 
-        True, sets the turns to the computer otherwise.
+        """Switch player's turn.
+
+        Game turn will change to the actual player if is_human is
+        True, sets the turn to the computer otherwise.
 
         """
         if self.is_human:
@@ -218,18 +201,15 @@ class Player():
             self.computer_turn()
 
     def human_turn(self) -> None:
-        """Totals up player 1's score of each round and prints out
-        the total score after the end of every round.
-
-        """
+        """Total up the score and print the total score after end of each round."""
         player_score = 0
+
         self.total_round += 1
 
-        # Continues the game if the player chooses to roll the
-        # dice again, ends the round otherwise.
+        # Continue the game if the player chooses to roll the dice again,
+        # ends the round otherwise.
         while True:
-            print("{:^20}\033[1mROUND: {}\033[0m\n".format(
-                '\n' * 3, self.total_round))
+            print("{:^20}\033[1mROUND: {}\033[0m\n".format('\n' * 3, self.total_round))
 
             print(f"({self.name})".center(25))
 
@@ -238,19 +218,19 @@ class Player():
             if roll_1 == 1 or roll_2 == 1:
                 print(f"\n\n{self.name} rolled a 1.")
                 break
-            elif roll_1 == 1 and roll_2 == 1:
+
+            if roll_1 == 1 and roll_2 == 1:
                 print(f"\n\n{self.name} rolled two 1s and lost all the scores.")
                 player_score = 0
                 break
-            else:
+
+            if roll_1 != 1 and roll_2 != 1:
                 print(f"\n\n{self.name} rolled a {roll_1} and {roll_2}.")
                 player_score += sum([roll_1, roll_2])
-                if player_score == 1 or player_score == 0:
-                    print(
-                        f"\n{self.name} scored \033[1m{player_score} point\033[0m in this round.\n\n{'-' * 23}")
+                if player_score in (0, 1):
+                    print(f"\n{self.name} scored \033[1m{player_score} point\033[0m in this round.\n\n{'-' * 23}")
                 else:
-                    print(
-                        f"\n{self.name} scored \033[1m{player_score} points\033[0m in this round.\n\n{'-' * 23}")
+                    print(f"\n{self.name} scored \033[1m{player_score} points\033[0m in this round.\n\n{'-' * 23}")
 
             if ask_user_yes_no("\nRoll again? (Y/N): "):
                 self.total_round += 1
@@ -263,11 +243,9 @@ class Player():
             f"\n\033[1m({self.name}) Total Score: {self.total_score}\033[0m\n\n{'=' * 30}\n")
 
     def computer_turn(self):
-        """Totals up the computer's score and prints out the 
-        total score after the end of each round.
-
-        """
+        """Total up the score and print out total score after the end of each round."""
         computer_score = 0
+
         self.total_round += 1
 
         print("{:^20}\033[1mROUND: {}\033[0m\n".format(
@@ -283,11 +261,13 @@ class Player():
             if roll_1 == 1 or roll_2 == 1:
                 print(f"\n\n{self.name} rolled a 1.")
                 break
-            elif roll_1 == 1 and roll_2 == 1:
+
+            if roll_1 == 1 and roll_2 == 1:
                 print(f"\n\n{self.name} rolled two 1s and lost all the scores.")
                 computer_score = 0
                 break
-            else:
+
+            if roll_1 != 1 and roll_2 != 1:
                 print(f"\n\n{self.name} rolled a {roll_1} and {roll_2}.")
                 computer_score += sum([roll_1, roll_2])
                 if computer_score < 20:
@@ -298,15 +278,14 @@ class Player():
         self.total_score += computer_score
         print(f"\n{self.name}'s turn ends.")
         print(f"\n{self.name} scored {computer_score} in this round.")
-        print(
-            f"\n\033[1m({self.name}) Total Score: {self.total_score}\033[0m\n\n{'=' * 30}\n")
+        print(f"\n\033[1m({self.name}) Total Score: {self.total_score}\033[0m\n\n{'=' * 30}\n")
 
 
 def should_play_again() -> bool:
-    """Asks the user if they want to play again.
+    """Ask the user if they want to play again.
 
     Returns:
-        True if the user wants to play 
+        True if the user wants to play
         again, False otherwise.
 
     """
@@ -315,9 +294,9 @@ def should_play_again() -> bool:
 
 
 def main():
-    """Starts the program.
+    """Start the program.
 
-    Restarts program if should_play_again function returns True, prints a 
+    Restarts program if should_play_again function returns True, prints a
     message telling the user that the program has exited otherwise.
 
     """
